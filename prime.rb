d8a976aadd4c5b6676c7a09da02f15ea43f0bab0
Yonnick Brown <25hour24@gmail.com>
2019-01-29 00:24:20 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,11 @@
 def prime?(num)
-  return false if num < 2
+  if num < 2
+    false
+  else
   (2..num -1).each do |n|
-    if (num % n == 0)
+  if (num % n == 0)
       return false
     end
-    end
-    true
+  end
+  true
 end
